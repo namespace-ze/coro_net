5a89574e0595f2c80a8a38f991c0268b5d00e887
HuangShize-2343611 <huangze065@gmail.com>
2026-05-25 23:32:57 +0800

    chore: add .gitignore and untrack build/ artifacts
    GitHub language stats were dominated by CMake-generated Makefiles
    because the build/ directory was committed. Removing 148 build
    artifacts from the index and adding .gitignore so future builds
    stay out of version control.

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ilin/project/Mprpc/coro_net
-# Build directory: /home/ilin/project/Mprpc/coro_net/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
-subdirs("example")
--- build/example/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ilin/project/Mprpc/coro_net/example
-# Build directory: /home/ilin/project/Mprpc/coro_net/build/example
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
